7cd739066c4d8ff251c59e9252d6dd9c58dd6772
lizaaadrozd <elizavetadrozd266@gmail.com>
2024-10-04 19:14:24 +0300

    Added recipe model, controller with crud actions, index, show views, routes and migrations

---
+++ db/migrate/20241004115155_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[7.2]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.string :description
+      t.string :complexity
+      t.string :ingredients
+      t.boolean :is_vegan
+
+      t.timestamps
+    end
+  end
+end
