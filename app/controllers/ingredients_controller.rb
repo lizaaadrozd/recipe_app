6dfb7d986463ebfcc88af46bc4272220af22d622
lizaaadrozd <elizavetadrozd266@gmail.com>
2024-10-10 21:48:21 +0300

    Added actions to the ingredients_controller.rb and ingredients views

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,4 @@
 class IngredientsController < ApplicationController
-  def index
     def index
       @ingredients = Ingredient.all
     end
@@ -8,5 +7,42 @@ class IngredientsController < ApplicationController
       @ingredient = Ingredient.find(params[:id])
     end
 
-  end
+    def new
+      @ingredient = Ingredient.new
+    end
+
+    def create
+      @ingredient = Ingredient.new(ingredient_params)
+      if @ingredient.save
+        redirect_to @ingredient
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+      @ingredient = Ingredient.find(params[:id])
+    end
+
+    def update
+      @ingredient = Ingredient.find(params[:id])
+
+      if @ingredient.update(ingredient_params)
+        redirect_to @ingredient
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      @ingredient = Ingredient.find(params[:id])
+      @ingredient.destroy
+      redirect_to root_path, status: :see_other
+    end
+
+    private
+
+    def ingredient_params
+      params.require(:ingredient).permit(:name, :quantity)
+    end
 end
